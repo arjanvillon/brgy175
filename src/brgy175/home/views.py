21eac0ed901233b2de1e54b18f1313e047f527b7
arjanlangit <heaven.arjan@gmail.com>
2019-10-08 15:12:37 +0800

    removed testblock

--- src/brgy175/home/views.py
+++ src/brgy175/home/views.py
@@ -6,6 +6,4 @@ from django.utils.timezone import utc
 def base(request):
     return render(request, 'home/base.html', {'title':'Dashboard'})
 
-def testBlock(request):
-    return render(request, 'home/testBlock.html', {'title':'Forms'})
 
